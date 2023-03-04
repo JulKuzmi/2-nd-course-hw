766f115c75597aa085bc0a9ebff14122290bf3da
JulKuzmi <enchantress000@mail.ru>
2023-03-04 22:48:13 +0300

    maked tasks 4,5,8

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ let name ='Brendan Eich';
 alert('Brendan Eich');
 
 //задача4
+let one =10;
+let two =2;
+alert(10+2);
+alert(10-2);
+alert(10*2);
+alert(10/2);
+
+//задача5
+let b =2;
+let result =2**5;
+alert(2**5);
+
+//задача6
+//задача7
+//задача8
+let age =prompt('Сколько вам лет?');
+alert(age);
+
