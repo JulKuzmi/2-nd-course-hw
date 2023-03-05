4ed6ce1fc40b077d801d5e51abca044477ca0b11
JulKuzmi <enchantress000@mail.ru>
2023-03-05 13:11:10 +0300

    completed 1 tasks and make tasks 6-10

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //задача1
 let a =10;
 alert(10);
-//
+alert(20);
 
 //задача2
 let year = 2007;
@@ -25,8 +25,45 @@ let result =2**5;
 alert(2**5);
 
 //задача6
+let d =9;
+let f =2;
+alert(9%2);
+
 //задача7
+let num =1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num ++;
+num --;
+alert(num);
+
 //задача8
 let age =prompt('Сколько вам лет?');
 alert(age);
 
+//задача9.0, 9.1, 9.2, 9.3
+const user ={
+    name: 'Julia',
+    age: 22,
+    isAdmin: true,
+};
+
+//добавляем новое свойство
+user.cityOfResidence = true;
+// изменяем возраст
+user.age = 23;
+
+console.log(user)
+
+delete user.cityOfResidence;
+
+//задача 9.4
+
+let info = prompt("Какую информацию хотите узнать о пользователе?", user);
+alert(user[info]);
+
+//задание 10
+let userName = prompt('Введите как вас зовут?');
+alert('Привет,'+ userName);
